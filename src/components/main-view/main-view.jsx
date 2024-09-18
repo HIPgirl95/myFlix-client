fdfac2893ace03e1b6a7e0cbc3f9429b8b82d0a6
HIPgirl95 <hannah@dawne.com>
2024-09-18 17:12:01 -0400

    adding movie

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -3,7 +3,25 @@ import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState([
+    {
+      Genre: {
+        Name: "Fantasy",
+        Description:
+          "A genre often set in an imaginary universt without any locations, events, or people from the real world.",
+      },
+      Director: {
+        Name: "Steven Spielberg",
+        Bio: "Steven Allan Spielberg is an American filmmaker and the most successful director in film history.",
+      },
+      Actors: [],
+      _id: "66b90f36cd0e090e39228fb5",
+      Title: "Hook",
+      Description:
+        "When his young children are abducted by his old nemesis, Captain Hook, middle-aged lawyer Peter Banning must return to his magical origins as Peter Pan.",
+      LeadActor: "Robin Williams",
+    },
+  ]);
 
   const [selectedMovie, setSelectedMovie] = useState(null);
 
@@ -24,7 +42,7 @@ export const MainView = () => {
     <div>
       {movies.map((movie) => (
         <MovieCard
-          key={movie.id}
+          key={movie._id}
           movie={movie}
           onMovieClick={(newSelectedMovie) => {
             setSelectedMovie(newSelectedMovie);
