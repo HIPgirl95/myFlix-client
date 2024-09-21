145267495b79232d938a43426c75b7a63e3a0a20
HIPgirl95 <hannah@dawne.com>
2024-09-21 17:34:35 -0400

    adding required PropTypes

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
     <div id="main-view-div">
@@ -11,4 +13,19 @@ export const MovieCard = ({ movie, onMovieClick }) => {
       </span>
     </div>
   );
+};
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Description: PropTypes.string.isRequired,
+    }).isRequired,
+    Director: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Bio: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired,
 };
